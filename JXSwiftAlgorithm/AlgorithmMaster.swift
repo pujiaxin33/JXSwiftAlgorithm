06e6f4baadbcb106500733eda699feb91c5f38fb
pjx <pujx@jingbo360.com>
2018-11-08 11:00:53 +0800

    first commit

---
+++ JXSwiftAlgorithm/AlgorithmMaster.swift
@@ -0,0 +1,65 @@
+//
+//  AlgorithmMaster.swift
+//  JXSwiftAlgorithm
+//
+//  Created by jiaxin on 2018/11/8.
+//  Copyright © 2018 jiaxin. All rights reserved.
+//
+
+import Foundation
+
+class AlgorithmMaster {
+
+    //对于题目的思考可以按步找到关键点，比如需要调整行列，跟目标值进行对比，不同的结果需要做出不同的调整。
+    //通过按步思考之后，应当抽象出关键的操作。比如等于就是找到了，大于就列数减少，小于就行数增加。
+    static func testTwoDimensionArrayFind() {
+        let numbers = [[1, 2, 8, 9],
+                       [2, 4, 9, 12],
+                       [4, 7, 10, 13],
+                       [6, 8, 11, 15]]
+        let targetNumber = 11
+        let maxColumn = numbers.first!.count - 1
+        let maxRow = numbers.count - 1
+        //从右往左比对
+        var currentColumn = maxColumn
+        var currentRow = 0
+        /* 自己按步思考的方案
+        while numbers[currentRow][currentColumn] > targetNumber  {
+            currentColumn -= 1
+        }
+        while numbers[currentColumn][currentRow] < targetNumber {
+            currentRow += 1
+        }
+        while currentColumn >= 0 && numbers[currentRow][currentColumn] != targetNumber   {
+            currentColumn -= 1
+        }
+        if currentRow >= numbers.count || currentColumn < 0 {
+            print("not found")
+            return
+        }
+        if numbers[currentRow][currentColumn] == targetNumber {
+            print("find number on \(currentRow):\(currentColumn)")
+        }else {
+            print("not found")
+        }
+         */
+        var isExist = false
+        while currentRow <= maxRow && currentColumn >= 0 {
+            //确定行列的边界
+            //根据对比的三种结果做出对应的调整
+            if numbers[currentRow][currentColumn] == targetNumber {
+                print("find number on \(currentRow):\(currentColumn)")
+                isExist = true
+                break
+            }else if numbers[currentRow][currentColumn] > targetNumber {
+                currentColumn -= 1
+            }else {
+                currentRow += 1
+            }
+        }
+        if !isExist {
+            print("not found")
+        }
+    }
+
+}
--- JXSwiftAlgorithm/ViewController.swift
+++ JXSwiftAlgorithm/ViewController.swift
@@ -8,13 +8,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum AlgorithmType {
+    case twoDimensionArrayFind
+}
+
+struct AlgorithmCellModel {
+    var name: String?
+    var type: AlgorithmType?
+}
+
+class ViewController: UITableViewController {
+    var dataSource = [AlgorithmCellModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        dataSource = [AlgorithmCellModel(name: "二维数组查找", type: .twoDimensionArrayFind)]
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+}
+
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cellModel = dataSource[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = cellModel.name
+        return cell
+    }
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cellModel = dataSource[indexPath.row]
+        switch cellModel.type! {
+        case .twoDimensionArrayFind:
+            AlgorithmMaster.testTwoDimensionArrayFind()
+        }
+    }
 }
 

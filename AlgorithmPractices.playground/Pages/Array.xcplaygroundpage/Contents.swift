df1293d5be29fcb6ff2488c18197891e4434d82c
Jason <317437084@qq.com>
2024-08-05 10:59:45 +0800

    add Array algorithm

---
+++ AlgorithmPractices.playground/Pages/Array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,166 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// 给你一个长度为 n 的整数数组，每次操作将会使 n - 1 个元素增加 1 。返回让数组所有元素相等的最小操作次数。
+func minMoves(_ nums: [Int]) -> Int {
+    let min = nums.min() ?? 0
+    var result = 0
+    for num in nums {
+        result += num - min
+    }
+    return result
+}
+
+//minMoves([1,1000000000])
+//minMoves([1,2,3])
+minMoves([-1,1])
+
+//88. 合并两个有序数组
+//给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+//
+//请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+//
+//注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+//示例 1：
+//
+//输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+//输出：[1,2,2,3,5,6]
+//解释：需要合并 [1,2,3] 和 [2,5,6] 。
+//合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+//示例 2：
+//
+//输入：nums1 = [1], m = 1, nums2 = [], n = 0
+//输出：[1]
+//解释：需要合并 [1] 和 [] 。
+//合并结果是 [1] 。
+//示例 3：
+//
+//输入：nums1 = [0], m = 0, nums2 = [1], n = 1
+//输出：[1]
+//解释：需要合并的数组是 [] 和 [1] 。
+//合并结果是 [1] 。
+//注意，因为 m = 0 ，所以 nums1 中没有元素。nums1 中仅存的 0 仅仅是为了确保合并结果可以顺利存放到 nums1 中。
+func testMergeTwoSortedArray() {
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        if n == 0 {
+            return
+        }
+        
+        let totalIndex = m + n
+        for index in m..<totalIndex {
+            nums1[index] = nums2[index - m]
+        }
+        // 从m的index开始做插入排序
+        for index in m..<totalIndex {
+            var i = index
+            while i >= 1 {
+                if nums1[i - 1] > nums1[i] {
+                    nums1.swapAt(i - 1, i)
+                    i -= 1
+                } else {
+                    break
+                }
+            }
+        }
+    }
+    
+    func merge2(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var sortedNums = Array<Int>(repeating: 0, count: m + n)
+        var i = 0
+        var j = 0
+        var index = 0
+        while i < m && j < n {
+            if nums1[i] <= nums2[j] {
+                sortedNums[index] = nums1[i]
+                index += 1
+                i += 1
+            } else {
+                sortedNums[index] = nums2[j]
+                index += 1
+                j += 1
+            }
+        }
+        while i < m {
+            sortedNums[index] = nums1[i]
+            index += 1
+            i += 1
+        }
+        while j < n {
+            sortedNums[index] = nums2[j]
+            index += 1
+            j += 1
+        }
+        nums1 = sortedNums
+    }
+    
+    func merge22(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var sortedNums = Array<Int>(repeating: 0, count: m + n)
+        var p1 = 0
+        var p2 = 0
+        var currentIndex = 0
+        var currentNum = 0
+        while p1 < m || p2 < n {
+            if p1 == m {
+                currentNum = nums2[p2]
+                p2 += 1
+            } else if p2 == n {
+                currentNum = nums1[p1]
+                p1 += 1
+            } else if nums1[p1] <= nums2[p2] {
+                currentNum = nums1[p1]
+                p1 += 1
+            } else {
+                currentNum = nums2[p2]
+                p2 += 1
+            }
+            sortedNums[currentIndex] = currentNum
+            currentIndex += 1
+        }
+        nums1 = sortedNums
+    }
+    
+    // 逆向双指针
+    func merge3(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var p1 = m - 1
+        var p2 = n - 1
+        var trail = m + n - 1
+        var sortedNums = Array<Int>.init(repeating: 0, count: m + n)
+        var currentNum = 0
+        while p1 >= 0 || p2 >= 0 {
+            if p1 == -1 {
+                currentNum = nums2[p2]
+                p2 -= 1
+            } else if p2 == -1 {
+                currentNum = nums1[p1]
+                p1 -= 1
+            } else if nums1[p1] > nums2[p2] {
+                currentNum = nums1[p1]
+                p1 -= 1
+            } else {
+                currentNum = nums2[p2]
+                p2 -= 1
+            }
+            sortedNums[trail] = currentNum
+            trail -= 1
+        }
+        nums1 = sortedNums
+    }
+    
+    var nums1 = [1,2,3,0,0,0]
+    let nums2 = [2,5,6]
+    merge22(&nums1, 3, nums2, 3)
+    print(nums1)
+    
+    var nums21 = [1]
+    merge22(&nums21, 1, [], 0)
+    print(nums21)
+    
+    var nums31 = [0]
+    merge22(&nums31, 0, [1], 1)
+    print(nums31)
+}
+testMergeTwoSortedArray()
+
+//: [Next](@next)
+

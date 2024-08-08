caf8f426ddd505fcb2c4ea5eb48a393bd65672de
Jason <317437084@qq.com>
2024-08-08 10:19:55 +0800

    remove duplicates 26

--- AlgorithmPractices.playground/Pages/Array.xcplaygroundpage/Contents.swift
+++ AlgorithmPractices.playground/Pages/Array.xcplaygroundpage/Contents.swift
@@ -207,5 +207,71 @@ func testRemoveElement27() {
 }
 testRemoveElement27()
 
+//给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+//
+//考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+//
+//更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
+//返回 k 。
+func testRemoveDuplicates26() {
+    func removeDuplicates(_ nums: inout [Int]) -> Int {
+        guard nums.count > 1 else {
+            return nums.count
+        }
+        var leftIndex = 0
+        var set = Set<Int>()
+        for index in 0..<nums.count {
+            if !set.contains(nums[index]) {
+                nums[leftIndex] = nums[index]
+                set.insert(nums[index])
+                leftIndex += 1
+            }
+        }
+        
+        return leftIndex
+    }
+    
+    func removeDuplicates2(_ nums: inout [Int]) -> Int {
+        guard nums.count > 1 else {
+            return nums.count
+        }
+        var leftIndex = 0
+        var map: [Int: Int] = [:]
+        for index in 0..<nums.count {
+            if map[nums[index]] == nil {
+                nums[leftIndex] = nums[index]
+                map[nums[index]] = 1
+                leftIndex += 1
+            }
+        }
+        
+        return leftIndex
+    }
+    
+    func removeDuplicates3(_ nums: inout [Int]) -> Int {
+        guard nums.count > 1 else {
+            return nums.count
+        }
+        
+        var fast = 1
+        var slow = 1
+        while fast < nums.count {
+            if nums[fast] != nums[fast - 1] {
+                //找到了边界
+                nums[slow] = nums[fast]
+                slow += 1
+            }
+            fast += 1
+        }
+        return slow
+    }
+    
+//    var nums = [0,0,1,1,1,2,2,3,3,4,5,6]
+    var nums = [1,2]
+    removeDuplicates3(&nums)
+    print(nums)
+}
+testRemoveDuplicates26()
+
 //: [Next](@next)
 

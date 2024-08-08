d6d737d73a92064dfc8c67272d08a0b91d417ed1
Jason <317437084@qq.com>
2024-08-08 09:48:34 +0800

    remove element

--- AlgorithmPractices.playground/Pages/Array.xcplaygroundpage/Contents.swift
+++ AlgorithmPractices.playground/Pages/Array.xcplaygroundpage/Contents.swift
@@ -162,5 +162,50 @@ func testMergeTwoSortedArray() {
 }
 testMergeTwoSortedArray()
 
+// 27. 移除元素
+//给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素。元素的顺序可能发生改变。然后返回 nums 中与 val 不同的元素的数量。
+//
+//假设 nums 中不等于 val 的元素数量为 k，要通过此题，您需要执行以下操作：
+//
+//更改 nums 数组，使 nums 的前 k 个元素包含不等于 val 的元素。nums 的其余元素和 nums 的大小并不重要。
+//返回 k。
+
+func testRemoveElement27() {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        guard nums.count > 0 else {
+            return 0
+        }
+        //思路：双指针。leftIndex只要等于val，就把rightIndex的值交换过来（不管右边的值是否等于val）
+        var leftIndex = 0
+        var rightIndex = nums.count - 1
+        //循环之前设定终止条件
+        while leftIndex <= rightIndex {
+            if nums[leftIndex] == val {
+                nums[leftIndex] = nums[rightIndex]
+                rightIndex -= 1
+            } else {
+                leftIndex += 1
+            }
+        }
+        return leftIndex
+    }
+    func removeElement2(_ nums: inout [Int], _ val: Int) -> Int {
+        //遍历整个数组，leftIndex就是等于val的值，被遍历的值只要不等于val，就填充到leftIndex
+        var leftIndex = 0
+        for index in 0..<nums.count {
+            if nums[index] != val {
+                nums[leftIndex] = nums[index]
+                leftIndex += 1
+            }
+        }
+        return leftIndex
+    }
+    
+    var nums = [3,2,2,3]
+    removeElement2(&nums, 3)
+    print(nums)
+}
+testRemoveElement27()
+
 //: [Next](@next)
 
